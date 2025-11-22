e246fa2477217d73fea031a57953aa65b2326e95
Nikolay Lazarov <nikolay.lazarov@vay.io>
2025-11-22 23:40:11 +0200

    update vim test config

--- vim/lua/plugins/testing.lua
+++ vim/lua/plugins/testing.lua
@@ -6,14 +6,26 @@ return {
             "nvim-lua/plenary.nvim",
             "antoinemadec/FixCursorHold.nvim",
             "nvim-treesitter/nvim-treesitter",
-            { "fredrikaverpil/neotest-golang", version = "*" }, -- Installation
+            {
+                "fredrikaverpil/neotest-golang",
+                version = "*",
+                build = function()
+                  vim.system({"go", "install", "gotest.tools/gotestsum@latest"}):wait()
+                end,
+            },
         },
         enabled = true,
         config = function()
             require("neotest").setup({
                 consumers = {
                     notify = function(client)
-                        client.listeners.results = function(_, results)
+                        client.listeners.run = function(_, _, _)
+                            vim.notify(("🏃🏻‍♂️‍➡️ Tests are running..."), vim.log.levels.INFO)
+                        end
+                        client.listeners.results = function(_, results, partial)
+                            if partial then
+                                return
+                            end
                             local total, passed, failed = 0, 0, 0
                             for _, result in pairs(results) do
                                 if result.status == "passed" then
@@ -24,9 +36,9 @@ return {
                                 total = total + 1
                             end
                             if failed > 0 then
-                                vim.notify(("❌ Tests failed: %d/%d"):format(failed, total), vim.log.levels.ERROR)
+                                vim.notify(("🔴 Tests failed: %d/%d"):format(failed, total), vim.log.levels.ERROR)
                             else
-                                vim.notify(("✅ All %d tests passed"):format(total), vim.log.levels.INFO)
+                                vim.notify(("🟢 All %d tests passed"):format(total), vim.log.levels.INFO)
                             end
                         end
                     end,
@@ -35,6 +47,7 @@ return {
                     require('rustaceanvim.neotest'),
                     require("neotest-golang")({
                         testify_enabled = true,
+                        runner = "gotestsum",
                     }),
                 },
             })
