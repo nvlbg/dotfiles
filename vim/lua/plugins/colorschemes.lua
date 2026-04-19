658dcd91a809a60b980bfdfc75b7d687bad2dbee
Nikolay Lazarov <nikolay.lazarov@vay.io>
2026-04-19 19:16:08 +0300

    fix colorscheme after nvim 0.12 update

--- vim/lua/plugins/colorschemes.lua
+++ vim/lua/plugins/colorschemes.lua
@@ -5,6 +5,7 @@ return {
             require('base16-colorscheme').with_config({
                 telescope = false,
             })
+            vim.opt.termguicolors = true
             vim.cmd.colorscheme('base16-circus')
         end,
     },
